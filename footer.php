d2dc273b85b10f38365b9411472cfb6e525ce669
garyfung221 <66684175+garyfung221@users.noreply.github.com>
2022-09-05 03:48:06 +0800

    Adding a office location

--- footer.php
+++ footer.php
@@ -1,6 +1,10 @@
 <!-- Footer -->
 <div class="wrapper row3">
   <footer id="footer" class="clear">
+  <p class="fl_left">Office Location - East Kowloon Smart Identity Card Replacement Centre</a></p>
+    
+    <br>
+    <br>
     <p class="fl_left">Copyright &copy; 2022 - All Rights Reserved - Immigration Department of Hong Kong Government<a href="#"></a></p>
   </footer>
 </div>
