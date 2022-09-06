a86d85c7a3e5f00590c99d62f54c53d75f13e1c1
garyfung221 <66684175+garyfung221@users.noreply.github.com>
2022-09-06 21:38:33 +0800

    Calendar appointment of status function
    input the date of number then calculate the remaining quotes of that day then display different colour on the  calendar

---
+++ calendar_calculatequota_process.php
@@ -0,0 +1,35 @@
+<?php
+//Connect database
+include "db.php";
+
+
+
+
+function calquotas($date){
+
+global $conn;
+
+$date = "%$date";
+
+$sql = $conn->prepare("SELECT * FROM appointment_quotas WHERE app_quotas_date LIKE ?");
+$sql->bind_param("s",$date);
+$sql->execute();
+$result=$sql->get_result();
+
+while($row = $result->fetch_assoc()){
+    if($row['app_quotas']==0){
+        return "full_background";
+    }else if($row['app_quotas']==8){
+            return "almostfull_background";
+        }else{
+            return "available_background";
+        }
+        
+    }
+}
+
+
+?>
+
+
+
